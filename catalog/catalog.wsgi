32b5f7d5cd4e15d9aa435a0949fbdadf7745fc68
Oba-One <obaone01@gmail.com>
2018-08-15 20:36:48 -0700

    Updated files

--- catalog/catalog.wsgi
+++ catalog/catalog.wsgi
@@ -4,5 +4,5 @@ import logging
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"/var/www/FlaskApp/catalog/")
 
-from catalog import app as application
+from __init__ import app as application
 
--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -54,7 +54,7 @@ class MenuItem(Base):
             'course': self.course,
         }
 
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
  
 
 Base.metadata.create_all(engine)
